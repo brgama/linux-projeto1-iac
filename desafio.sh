deaea4172f1b7a135423b3f41f4b75d76dbf0d8f
Bruno da Gama Santos <bruno.gama.ter@estacio.br>
2022-10-14 18:38:41 -0300

    commit desafio

---
+++ desafio.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Começando o desafio..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -G GRP_SEC
+useradd rogerio -m -G GRP_SEC
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "FIM"
